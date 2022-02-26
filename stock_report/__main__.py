6a87164a3b89a8500334abb6c4216bc4b7b60222
pfeiffer-dev <84971615+pfeiffer-dev@users.noreply.github.com>
2022-02-26 19:05:27 +0100

    -

--- stock_report/__main__.py
+++ stock_report/__main__.py
@@ -23,56 +23,56 @@ while True:
         
         d = data.DATA(name=name, ticker="AAPL") # Create object data for search
         
-        #try:
-        print(d.symbol_search(keyword=name)) # Print searche
-        print("")
-                
-        print("What's the company's ticker?")
-        ticker = str(input(">>> "))
-        print("")
-                
-        print('One moment please, PDF is being generated...')
-        print("")
+        try:
+                print(d.symbol_search(keyword=name)) # Print searche
+                print("")
                         
-        d = data.DATA(name=name, ticker=ticker) # Create object data
-        a = analysis.ANALYSIS(data=d) # Create object analysis
+                print("What's the company's ticker?")
+                ticker = str(input(">>> "))
+                print("")
                         
-        a.main() # run analysis
-                        
-        p = pdf.PDF(data=d, name=name, ticker=ticker) # Create object pdf
-                        
-        p.new_page()
-        p.create_title()
-        p.key_figures()
-                        
-        time.sleep(60) # time sleep for alpha vantage api
-                
-        p.new_page()
-        p.create_heading("Income Statement")
-        p.income_statement()
-                        
-        p.new_page()
-        p.create_heading("Balance Sheet")
-        p.balance_sheet()
-                        
-        p.new_page()
-        p.create_heading("Cashflow")
-        p.cash_flow()
-                        
-        p.new_page()
-        p.create_heading("Technical Analysis")
-        p.technical_analysis()
-
-        p.pdf.output(f'{name}.pdf', 'F') # Create pdf
-                
-        download_folder = os.path.expanduser("~")+"/Downloads/"
-        shutil.move(f'{name}.pdf', download_folder) # Move pdf to output folder
-        
+                print('One moment please, PDF is being generated...')
+                print("")
                                 
-        print('PDF is ready!!!')
-        print("You can quit with ctrl + c everytime.")
-        print("")
-        #except:
-               # print("Something must have gone wrong :(")
-               # print("Try again or quit with ctrl + c")
-               # print("")  
+                d = data.DATA(name=name, ticker=ticker) # Create object data
+                a = analysis.ANALYSIS(data=d) # Create object analysis
+                                
+                a.main() # run analysis
+                                
+                p = pdf.PDF(data=d, name=name, ticker=ticker) # Create object pdf
+                                
+                p.new_page()
+                p.create_title()
+                p.key_figures()
+                                
+                time.sleep(60) # time sleep for alpha vantage api
+                        
+                p.new_page()
+                p.create_heading("Income Statement")
+                p.income_statement()
+                                
+                p.new_page()
+                p.create_heading("Balance Sheet")
+                p.balance_sheet()
+                                
+                p.new_page()
+                p.create_heading("Cashflow")
+                p.cash_flow()
+                                
+                p.new_page()
+                p.create_heading("Technical Analysis")
+                p.technical_analysis()
+
+                p.pdf.output(f'{name}.pdf', 'F') # Create pdf
+                        
+                download_folder = os.path.expanduser("~")+"/Downloads/"
+                shutil.move(f'{name}.pdf', download_folder) # Move pdf to output folder
+                
+                                        
+                print('PDF is ready!!!')
+                print("You can quit with ctrl + c everytime.")
+                print("")
+        except:
+               print("Something must have gone wrong :(")
+               print("Try again or quit with ctrl + c")
+               print("")  
